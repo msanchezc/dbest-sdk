9c60dbd0a49540d9c29f523d9a6bbb6a87574157
Carlos Tovar <cartovarc@gmail.com>
2020-08-03 16:47:45 -0500

    add DbestRequest

--- dbest_sdk/core.py
+++ dbest_sdk/core.py
@@ -5,6 +5,8 @@ import time
 from threading import Lock
 from dbest_sdk.autogen import bidirectional_pb2_grpc as bidirectional_pb2_grpc
 from dbest_sdk.autogen import bidirectional_pb2 as bidirectional_pb2
+from dbest_sdk.states import DbesState
+from dbest_sdk.messages import DbestRequest, DbestResponse
 
 
 class Dbest:
@@ -26,7 +28,12 @@ class Dbest:
         response = stub.SimpleRequest(
             Dbest._build_simple_request_message(data_str), timeout=10
         )
-        time.sleep(0.5)  # TODO remove sleep
+        time.sleep(1)  # TODO remove sleep, bug #1
+        return response
+
+    def _response_from_request(self, dbest_request):
+        response_str = self._simple_request(dbest_request.value)
+        response = DbestResponse(int(response_str))
         return response
 
     def connect(self):
@@ -34,109 +41,6 @@ class Dbest:
         Allow you connect your instance to server running on DBEST
         """
         self.channel = grpc.insecure_channel(self.ip)
-
-    def lock(self):
-        """
-        Lock Dbest, in this state no request is received.
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def unlock(self):
-        """
-        Unlock Dbest. DANGER: before calling this method it is necessary to remove all the drones from the exchanger manually. This method will reset Dbest to its initial state.
-
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def get_current_state(self):
-        """
-        Gets the current state of Dbest
-        Returns:
-            [DbesState]: Current Dbest state.
-        """
-        pass
-
-    def get_drone_count(self):
-        """
-        Gets total drone count inside Dbest, returned value can be 0, 1 or 2.
-        Returns:
-            [int]: drone count
-        """
-        pass
-
-    def take_drone_out(self):
-        """
-        Take out a drone from exchange area or auxiliary area, deploy top plattform and moves the drone to takeoff area.
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def take_drone_in(self):
-        """
-        Retract top plattform and move the drone from takeoff area to exchange area.
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def prepare_to_descent(self):
-        """
-        Deploy bottom plattform (Biggest ARUCO)
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def prepare_to_landing(self):
-        """
-        Deploy top plattform (Smallest ARUCO)
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def exchange_battery(self, n):
-        """
-        If there are any drones in the exchange area, Dbest starts exchanging the drone battery with the battery in the socket with index n.
-        Args:
-            - n (int): Socket index [0, 7]
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def retract_bottom_platform(self):
-        """
-        Starts to retract buttom plaftform
-
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def retract_top_platform(self):
-        """
-        Starts to retract top plaftform
-
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
-
-    def set_socket_enabled(self, flag, n):
-        """
-        Enable / Disable battery socket with index n
-        Args:
-            - n (int): Socket index [0, 7]
-        Returns:
-            [DbestResponse]: Request response.
-        """
-        pass
 
     def is_socket_enabled(self, n):
         """
@@ -146,7 +50,7 @@ class Dbest:
         Returns:
             [boolean]: True / False if socket is enabled / disabled on index n
         """
-        pass
+        return self._response_from_request(DbestRequest.is_socket_enabled_request(n))
 
     def is_socket_cooling(self, n):
         """
@@ -156,7 +60,112 @@ class Dbest:
         Returns:
             [boolean]: True / False if socket is cooling / disabled on index n
         """
-        pass
+        return self._response_from_request(DbestRequest.is_socket_cooling_request(n))
+
+    def set_socket_enabled(self, flag, n):
+        """
+        Enable / Disable battery socket with index n
+        Args:
+            - n (int): Socket index [0, 7]
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(
+            DbestRequest.set_socket_enabled_request(flag, n)
+        )
+
+    def exchange_battery(self, n):
+        """
+        If there are any drones in the exchange area, Dbest starts exchanging the drone battery with the battery in the socket with index n.
+        Args:
+            - n (int): Socket index [0, 7]
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.exchange_battery_request(n))
+
+    def lock(self):
+        """
+        Lock Dbest, in this state no request is received.
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.LOCK)
+
+    def unlock(self):
+        """
+        Unlock Dbest. DANGER: before calling this method it is necessary to remove all the drones from the exchanger manually. This method will reset Dbest to its initial state.
+
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.UNLOCK)
+
+    def take_drone_out(self):
+        """
+        Take out a drone from exchange area or auxiliary area, deploy top plattform and moves the drone to takeoff area.
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.TAKE_DRONE_OUT)
+
+    def take_drone_in(self):
+        """
+        Retract top plattform and move the drone from takeoff area to exchange area.
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.TAKE_DRONE_OUT)
+
+    def prepare_to_descent(self):
+        """
+        Deploy bottom plattform (Biggest ARUCO)
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.PREPARE_TO_DESCENT)
+
+    def prepare_to_land(self):
+        """
+        Deploy top plattform (Smallest ARUCO)
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.PREPARE_TO_LAND)
+
+    def retract_bottom_platform(self):
+        """
+        Starts to retract buttom plaftform
+
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.RETRACT_BOTTOM_PLATTFORM)
+
+    def retract_top_platform(self):
+        """
+        Starts to retract top plaftform
+
+        Returns:
+            [DbestResponse]: Request response.
+        """
+        return self._response_from_request(DbestRequest.RETRACT_TOP_PLATTFORM)
+
+    def get_current_state(self):
+        """
+        Gets the current state of Dbest
+        Returns:
+            [DbesState]: Current Dbest state.
+        """
+        return self._response_from_request(DbestRequest.GET_CURRENT_STATE)
+
+    def get_drone_count(self):
+        """
+        Gets total drone count inside Dbest, returned value can be 0, 1 or 2.
+        Returns:
+            [int]: drone count
+        """
+        return self._response_from_request(DbestRequest.GET_DRONE_COUNT)
 
     def wait_for_state(self, status_to_wait):
         """
@@ -184,16 +193,14 @@ class StatusChangedListener:
         self.uid = str(uuid.uuid1())
 
     def _subscribe(self):
-        stub = bidirectional_pb2_grpc.BidirectionalStub(
-            self.dbest_instance.channel)
+        stub = bidirectional_pb2_grpc.BidirectionalStub(self.dbest_instance.channel)
         responses = stub.SubscribeStateListener(
             bidirectional_pb2.Subscribe(id=self.uid)
         )
         try:
             for new_state_response in responses:
-                t = threading.Thread(
-                    target=self.onStateChanged, args=[new_state_response.state]
-                )
+                state = DbesState(int(new_state_response.state))
+                t = threading.Thread(target=self.onStateChanged, args=[state])
                 t.daemon = True
                 t.start()
         except Exception as e:
@@ -214,8 +221,7 @@ class StatusChangedListener:
         When subscribe function is called,
         onStateChange will be called when a DBEST status change is notified.
         """
-        stub = bidirectional_pb2_grpc.BidirectionalStub(
-            self.dbest_instance.channel)
+        stub = bidirectional_pb2_grpc.BidirectionalStub(self.dbest_instance.channel)
         response = stub.UnsubscribeStateListener(
             bidirectional_pb2.Unsubscribe(id=self.uid)
         )
---
+++ dbest_sdk/messages.py
@@ -0,0 +1,64 @@
+import enum
+
+# Get data from Dbest
+# GTXXYYYYYYYY...
+# Examples:
+# GTSE6 ->  Get Socket enabled for socket 6
+# GTSC0 ->  Get Socket cooling for socket 0
+# GTCS -> Get current dbest state
+# GTDC -> Get drone count inside Dbest
+
+# Set some data to Dbest
+# STXXYYYYYYYY...
+# Examples:
+# STSET7 -> Set enabled to True to socket with index 7 
+# STSEF2 -> Set enabled to False to socket with index 2
+
+class DbestRequest:
+    LOCK = "DRLO"
+    UNLOCK = "DRUL"
+    TAKE_DRONE_OUT = "DRTO"
+    TAKE_DRONE_IN = "DRTI"
+    PREPARE_TO_DESCENT = "DRPD"
+    PREPARE_TO_LAND = "DRPL"
+    RETRACT_BOTTOM_PLATTFORM = "DRRB"
+    RETRACT_TOP_PLATTFORM = "DRRT"
+    GET_CURRENT_STATE = "GTCS"
+    GET_DRONE_COUNT = "GTDC"
+
+    @staticmethod
+    def exchange_battery_request(n):
+        assert isinstance(n, int)
+        assert n >=0 and n <= 7
+        return "DREB%s" % n
+
+    @staticmethod
+    def set_socket_enabled_request(flag, n):
+        assert isinstance(flag, bool)
+        assert isinstance(n, int)
+        assert n >=0 and n <= 7
+        if flag:
+            c = "T"
+        else:
+            c = "F"
+        return "STSE%s%s" % (c, n)
+    
+    @staticmethod
+    def is_socket_cooling_request(n):
+        assert isinstance(n, int)
+        assert n >=0 and n <= 7
+        return "GTSC%s" % n
+
+    @staticmethod
+    def is_socket_enabled_request(n):
+        assert isinstance(n, int)
+        assert n >=0 and n <= 7
+        return "GTSE%s" % n
+
+class DbestResponse(enum.Enum):
+    ACK = 200
+    NOT_ALLOWED_IN_CURRENT_STATE = 400
+    INCONGRUITY_SENSOR_ON_AUXILIARY_AREA = 501
+    INCONGRUITY_SENSOR_OFF_AUXILIARY_AREA = 502
+    INCONGRUITY_SENSOR_ON_EXCHANGE_AREA = 503
+    INCONGRUITY_SENSOR_OFF_EXCHANGE_AREA = 504
