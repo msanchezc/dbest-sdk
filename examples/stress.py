cd4c0ac86082af11d4a9bc4e87082cb466758d0f
Carlos Tovar <cartovarc@gmail.com>
2020-09-04 16:14:54 -0500

    sleeps

--- examples/stress.py
+++ examples/stress.py
@@ -4,22 +4,26 @@ import dbest_sdk, time
 dbest_instance = dbest_sdk.Dbest(ip="localhost:50051")
 dbest_instance.connect()
 
-sleep_seconds = 5
+sleep_seconds = 10
 cont = 0
 res = dbest_instance._simple_request("02") # dbest_instance.unlock()
 print(res)
+time.sleep(sleep_seconds)
 
 while True:
     print("iteración %s" % cont)
 
     res = dbest_instance._simple_request("03") # dbest_instance.prepare_to_descent()
     print(res)
+    time.sleep(sleep_seconds)
 
     res = dbest_instance._simple_request("04") # dbest_instance.prepare_to_land()
     print(res)
+    time.sleep(sleep_seconds)
 
     res = dbest_instance._simple_request("05") # dbest_instance.take_drone_in()
     print(res)
+    time.sleep(sleep_seconds)
 
     if cont % 2 == 0:
         res = dbest_instance._simple_request("06") #dbest_instance.exchange_battery(1)
@@ -27,15 +31,16 @@ while True:
         res = dbest_instance._simple_request("07") #dbest_instance.exchange_battery(2)
     
     print(res)
+    time.sleep(sleep_seconds)
 
     cont = cont + 1
 
     res = dbest_instance._simple_request("08") #dbest_instance.take_drone_out()
     print(res)
+    time.sleep(sleep_seconds)
 
     res = dbest_instance._simple_request("09") #dbest_instance.retract_top_platform()
     print(res)
-
     time.sleep(sleep_seconds)
 
 dbest_instance.disconnect()
